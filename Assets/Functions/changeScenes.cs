1c85cf264ab5a0d4c95a214b209c7e65925d0174
izonn <izonn@naver.com>
2016-11-05 11:15:12 +0900

    MyDogMaker First Commit
    MyDogMaker First Commit

---
+++ Assets/Functions/changeScenes.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class changeScenes : MonoBehaviour {
+
+    public string sceneName;
+
+    void Start()
+    {
+        Debug.Log("Start");
+        ChangeScene("f");
+    }
+
+    public void ChangeScene (string nm) {
+        Debug.Log("화면 전환");
+        Debug.Log(sceneName.ToString());
+        //new WaitForSeconds(5);
+        SceneManager.LoadScene(sceneName.ToString());
+    }
+}
