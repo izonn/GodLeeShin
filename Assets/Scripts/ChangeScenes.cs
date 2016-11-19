9b4b17d6cb49dc7a9170df9e7ddd876e4f3cc51e
izonn <izonn@naver.com>
2016-11-19 12:13:55 +0900

    정리

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour {
+
+    public string sceneName;
+    public float waitSec = 0;
+
+    public void Start()
+    {
+        StartCoroutine(ChangeScene(waitSec, sceneName));
+    }
+
+    IEnumerator ChangeScene(float num, string sceneNm)
+    {
+        Debug.Log("==ChangeScene==");
+        Debug.Log("Scene Name : " + sceneNm);
+        Debug.Log("Waiting Seconds : " + num);
+
+        yield return new WaitForSeconds(num);
+        SceneManager.LoadScene(sceneNm);
+    }
+
+    public void ChangeScene(string sceneNm)
+    {
+        Debug.Log("==ChangeScene==");
+        Debug.Log("Scene Name : " + sceneNm);
+
+        SceneManager.LoadScene(sceneNm);
+    }
+}
