e5000639352509fa111352d2e6adcce90a6ed99b
lyncher48 <avidastudies@gmail.com>
2016-12-04 11:22:10 +0900

    하면할수록 헷갈린다. 모르겠다...

--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -4,30 +4,31 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScenes : MonoBehaviour {
 
-    public string sceneName;
+    public Object Scene;
+    //public string sceneName;
     public float waitSec = 0;
 
     public void Start()
     {
-        Screen.SetResolution(480, 800, true);
-        StartCoroutine(ChangeScene(waitSec, sceneName));
+        //Screen.SetResolution(480, 800, true);
+        StartCoroutine(ChangeScene(waitSec, Scene));
     }
 
-    IEnumerator ChangeScene(float num, string sceneNm)
+    public IEnumerator ChangeScene(float num, Object Scene)
     {
         Debug.Log("==ChangeScene==");
-        Debug.Log("Scene Name : " + sceneNm);
+        Debug.Log("Scene Name : " + Scene.name);
         Debug.Log("Waiting Seconds : " + num);
 
         yield return new WaitForSeconds(num);
-        SceneManager.LoadScene(sceneNm);
+        SceneManager.LoadScene(Scene.name);
     }
 
-    public void ChangeScene(string sceneNm)
+    public void ChangeScene(Scene Scene)
     {
         Debug.Log("==ChangeScene==");
-        Debug.Log("Scene Name : " + sceneNm);
+        Debug.Log("Scene Name : " + Scene.name);
 
-        SceneManager.LoadScene(sceneNm);
+        SceneManager.LoadScene(Scene.name);
     }
 }
