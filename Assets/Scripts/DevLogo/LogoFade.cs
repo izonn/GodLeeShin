4b30a118bb80ab3c0c5ec1351f11da5aa42f566f
lyncher48 <avidastudies@gmail.com>
2017-02-05 10:03:04 +0900

    170205 - 재커밋

---
+++ Assets/Scripts/DevLogo/LogoFade.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogoFade : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(DoFade());
+    }
+
+    IEnumerator DoFade()
+    {
+        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += Time.deltaTime * 2;
+            yield return null;
+        }
+        canvasGroup.interactable = true;
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/DevLogo/LogoManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogoManager : MonoBehaviour {
+
+    public static LogoManager instance = null;
+    public Canvas Logo_canvas;
+    public CanvasGroup Logo;
+
+    void Awake()
+    {
+
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        //Logo.alpha = 0;
+        Logo_canvas.enabled = false;
+    }
+
+}
